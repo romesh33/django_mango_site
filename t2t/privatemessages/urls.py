087a3a8c3a7449b63ac78b00e5e2bca10f12efcc
Roman Sheyko <r.sheyko@gmail.com>
2016-03-05 10:38:50 +0300

    Deleted private_messages app.

--- t2t/privatemessages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from . import views
-
-urlpatterns = [
-    url(r'^send_message/$', views.send_message_view, name='send_message_view'),
-    url(r'^send_message_api/(?P<thread_id>\d+)/$', views.send_message_api_view, name='send_message_api_view'),
-    url(r'^chat/(?P<thread_id>\d+)/$', views.chat_view, name='chat_view'),
-    url(r'^$', views.messages_view, name='messages_view'),
-]
